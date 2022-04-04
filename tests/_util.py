12a336686f0fbe1dff921cb7b994c6b12506499f
y-du <42994541+y-du@users.noreply.github.com>
2022-04-04 11:31:14 +0200

    init tests

---
+++ tests/_util.py
@@ -0,0 +1,49 @@
+"""
+   Copyright 2022 InfAI (CC SES)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import util
+import mf_lib
+import logging
+import json
+
+logger = logging.getLogger("operator")
+logger.disabled = True
+
+with open("tests/resources/mock_messages.json") as file:
+    mock_messages = json.load(file)
+
+
+def init_filter_handler(opr_config):
+    if not isinstance(opr_config, util.OperatorConfig):
+        opr_config = util.OperatorConfig(opr_config)
+    filter_handler = mf_lib.FilterHandler()
+    for it in opr_config.inputTopics:
+        filter_handler.add_filter(util.gen_filter(input_topic=it, selectors=opr_config.config.selectors))
+    return filter_handler
+
+
+class MockOperator(util.OperatorBase):
+    def func_1(self, a, timestamp):
+        assert a == mock_messages[0]["data"]["val_a"]
+        assert timestamp == mock_messages[0]["data"]["time"]
+
+    def func_2(self, a, b, timestamp):
+        assert a == mock_messages[1]["data"]["val_a"]
+        assert b == mock_messages[1]["data"]["val_b"]
+        assert timestamp == mock_messages[1]["data"]["time"]
+
+    def run(self, selector, data):
+        getattr(self, selector)(**data)
---
+++ tests/test_operator.py
@@ -0,0 +1,40 @@
+"""
+   Copyright 2022 InfAI (CC SES)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+from ._util import *
+import unittest
+
+
+class TestOperator(unittest.TestCase):
+    def test_call_run(self):
+        with open("tests/resources/mock_opr_config.json") as file:
+            mock_opr_config = json.load(file)
+        filter_handler = init_filter_handler(mock_opr_config)
+        mock_operator = MockOperator()
+        mock_operator.init(
+            kafka_consumer=None,
+            kafka_producer=None,
+            filter_handler=filter_handler
+        )
+        for message in mock_messages:
+            mock_operator._OperatorBase__call_run(message)
+
+    def test_run(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
