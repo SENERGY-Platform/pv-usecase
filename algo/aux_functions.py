268e67c58add4335c3834c3f88df46d08a2a3ba5
Christoph Schrade <schrade@infai.org>
2022-04-07 14:38:23 +0200

    refactor

--- algo/aux_functions.py
+++ algo/aux_functions.py
@@ -12,12 +12,3 @@ def load_weather_data():
     # Load and preprocess weather data here.
 
     return (time,(temp, humid, uv, rain_prob, cloud, time_since_rise, time_until_sunset))
-
-
-def update_history_power(history_power, new_power_value, history_time_delta): # Updates the history of the last power values.
-    if len(history_power) < history_time_delta:
-        history_power.append(new_power_value)
-    elif len(history_power) == history_time_delta:
-        history_power.append(new_power_value)
-        history_power = history_power[1:]
-    return history_power
