fix dimension in softmax

algo/Agent.py
@@ -20,7 +20,7 @@ class Policy(nn.Module):
 
     def forward(self, input):
         x = F.relu(self.fc1(input))
-        x = F.softmax(self.fc2(x), dim=1)
+        x = F.softmax(self.fc2(x), dim=0)
         return x
 
 