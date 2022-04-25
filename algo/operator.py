f9d3596a36b19e56982b5b96adb13e31868c7bce
franzmueller <franzmueller@users.noreply.github.com>
2022-04-25 15:14:39 +0200

    add debug log

--- algo/operator.py
+++ algo/operator.py
@@ -89,6 +89,8 @@ class Operator(util.OperatorBase):
             agent.update_power_list(new_power_value)
 
     def run(self, data, selector):
+        if os.getenv("DEBUG") is True:
+            print(selector + ": " + str(data))
         if selector == 'weather_func':
             if self.weather_same_timestamp != []:
                 if data['weather_time'] == self.weather_same_timestamp[-1]['weather_time']:
