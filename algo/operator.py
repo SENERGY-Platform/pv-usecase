27e688c8046724c263fe6894faefee3e254c18ef
Christoph Schrade <schrade@infai.org>
2022-04-28 14:06:22 +0200

    add loader for pretrained models

--- algo/operator.py
+++ algo/operator.py
@@ -54,6 +54,9 @@ class Operator(util.OperatorBase):
 
         self.model_file = f'{data_path}/{self.energy_src_id}_{self.weather_src_id}_model.pt'
 
+        if os.path.exists(self.model_file):
+            self.policy.load_state_dict(torch.load(self.model_file))
+
     def run_new_weather(self, new_weather_data):
         new_weather_array = aux_functions.preprocess_weather_data(new_weather_data)
         new_weather_input = np.mean(new_weather_array, axis=0)
