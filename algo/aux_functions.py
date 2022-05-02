5d98c03d12aa592b78c0ef6a774f0ad9728ab740
Christoph Schrade <schrade@infai.org>
2022-05-02 11:41:36 +0200

    add weather data normalization

--- algo/aux_functions.py
+++ algo/aux_functions.py
@@ -10,9 +10,12 @@ def preprocess_power_data(new_power_data):
 
 def preprocess_weather_data(new_weather_data):
 
-    aux_list = [[pd.to_datetime(data_point['weather_time']).tz_localize(None).hour, data_point['instant_air_temperature'], data_point['instant_relative_humidity'],
-                 data_point['instant_ultraviolet_index_clear_sky'], data_point['1_hours_precipitation_amount'],
-                 data_point['instant_cloud_area_fraction']] for data_point in new_weather_data]
+    # Normalization of time (min=0, max=24), relative humidity (min=0, max=100), uv-index (min=0, max~10), cloud area fraction
+    # (min=0, max=100) is easy. For temperature we choose min=-10, max=30 and for precipitation amount: min=0, max=10.
+
+    aux_list = [[pd.to_datetime(data_point['weather_time']).tz_localize(None).hour/24, (data_point['instant_air_temperature']+10)/40, data_point['instant_relative_humidity']/100,
+                 data_point['instant_ultraviolet_index_clear_sky']/10, data_point['1_hours_precipitation_amount']/10,
+                 data_point['instant_cloud_area_fraction']/100] for data_point in new_weather_data]
 
     weather_array = np.array(aux_list)
 
