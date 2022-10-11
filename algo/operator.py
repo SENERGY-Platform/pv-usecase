febc2bc815b4064419d1c84e9b66b832f2a400e6
Christoph Schrade <schrade.christoph@gmail.com>
2022-10-11 10:21:15 +0200

    create output only if training happened

--- algo/operator.py
+++ algo/operator.py
@@ -189,7 +189,8 @@ class Operator(util.OperatorBase):
                 output = self.run_new_weather(new_weather_data[0:3])
                 power_forecast = self.create_power_forecast(new_weather_data)
                 self.weather_same_timestamp = []
-                return [{'timestamp':timestamp.strftime('%Y-%m-%d %X')+'Z', 'value': probability} for timestamp, probability in power_forecast]
+                if len(self.replay_buffer)==self.buffer_len:
+                    return [{'timestamp':timestamp.strftime('%Y-%m-%d %X')+'Z', 'value': probability} for timestamp, probability in power_forecast]
         elif selector == 'power_func':
             self.run_new_power(data)
                 
