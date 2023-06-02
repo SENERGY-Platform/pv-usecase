8aa78a98eda6bcefc449a4761e7f06ea862316b4
Christoph Schrade <schrade.christoph@gmail.com>
2023-06-02 10:44:52 +0200

    include time as input variable

--- algo/aux_functions.py
+++ algo/aux_functions.py
@@ -15,7 +15,7 @@ def preprocess_weather_data(new_weather_data):
     # Normalization of relative humidity (min=0, max=100), uv-index (min=0, max~10), cloud area fraction
     # (min=0, max=100) is easy. For temperature we choose min=-10, max=30 and for precipitation amount: min=0, max=10.
 
-    aux_list = [[(data_point['instant_air_temperature']+10)/40, data_point['instant_relative_humidity']/100,
+    aux_list = [[pd.to_datetime(data_point['weather_time']).hour/24, (data_point['instant_air_temperature']+10)/40, data_point['instant_relative_humidity']/100,
                  data_point['instant_ultraviolet_index_clear_sky']/10, data_point['1_hours_precipitation_amount']/10,
                  data_point['instant_cloud_area_fraction']/100] for data_point in new_weather_data]
 
