4bfa2ae8f387da549b7b022e671d7ac2e2d0b765
Christoph Schrade <schrade.christoph@gmail.com>
2023-07-03 13:59:25 +0200

    save memory profile info to log file

--- algo/operator.py
+++ algo/operator.py
@@ -87,7 +87,9 @@ class Operator(util.OperatorBase):
                 if os.path.getsize(self.num_learned_from_buffer_file) > 0:
                     self.num_learned_from_buffer = pickle.load(f)
 
-    @profile
+    fp = open("opt/data/memory_profile_logs.log","w+")
+
+    @profile(stream=fp)
     def run_new_weather(self, new_weather_data):
         weather_time, new_weather_array = aux_functions.preprocess_weather_data(new_weather_data)
         new_weather_input = np.mean(new_weather_array, axis=0)
